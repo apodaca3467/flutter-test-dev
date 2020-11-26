b09cfbaf0e031a6ac110033d8504ce54a08bd791
Jesús Apodaca <a354631@uabc.edu.mx>
2020-11-26 13:09:10 -0800

    Proyecto entregable

--- flutter_test_dev/lib/main.dart
+++ flutter_test_dev/lib/main.dart
@@ -1,3 +1,7 @@
+// Desarrollador: Jesús Ruiz Apodaca
+// Fecha: 26 de noviembre de 2020
+//Flutter Dev Test
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,7 +17,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Home(title: 'Flutter Dev Test'),
+      home: Home(title: 'Disponible en tu zona'),
     );
   }
 }
@@ -30,38 +34,44 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: ListView(
-          children: <Widget>[
-            tarjetas(
-                "https://media-cdn.tripadvisor.com/media/photo-s/08/e6/df/49/mr-pampas-do-brasil-ensenada.jpg",
-                "Pampas Do Brasil",
-                false),
-            tarjetas(
-                "https://www.race-dezert.com/home/wp-content/uploads/2016/11/baja-1000-taco-stand-ensenada-6854.jpg",
-                "Tacos Angelito",
-                true),
-            tarjetas(
-                "https://10619-2.s.cdn12.com/rests/original/322_508588766.jpg",
-                "Victors Pizza",
-                true),
-            tarjetas(
-                "https://menu.sluurpy.com/immagini/locali/thumbs/5012602.jpg",
-                "Caffe Tomás",
-                false),
-            tarjetas(
-                "https://media-cdn.tripadvisor.com/media/photo-s/0c/18/30/21/starbucks.jpg",
-                "Starbucks MacroPlaza del Mar",
-                true),
-          ],
-        ));
-    // );
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView(
+        children: <Widget>[
+          tarjetas(
+              "Pampas Do Brasil",
+              "https://media-cdn.tripadvisor.com/media/photo-s/08/e6/df/49/mr-pampas-do-brasil-ensenada.jpg",
+              false,
+              context),
+          tarjetas(
+              "Tacos Angelito",
+              "https://www.race-dezert.com/home/wp-content/uploads/2016/11/baja-1000-taco-stand-ensenada-6854.jpg",
+              true,
+              context),
+          tarjetas(
+              "Victors Pizza",
+              "https://10619-2.s.cdn12.com/rests/original/322_508588766.jpg",
+              true,
+              context),
+          tarjetas(
+              "Caffe Tomás",
+              "https://menu.sluurpy.com/immagini/locali/thumbs/5012602.jpg",
+              false,
+              context),
+          tarjetas(
+              "Starbucks MacroPlaza del Mar",
+              "https://media-cdn.tripadvisor.com/media/photo-s/0c/18/30/21/starbucks.jpg",
+              true,
+              context),
+        ],
+      ),
+    );
   }
 }
 
-Widget tarjetas(String pURL, String pNombre, bool pEstado) {
+Widget tarjetas(
+    String pNombre, String pURL, bool pEstado, BuildContext context) {
   return Card(
     shape: RoundedRectangleBorder(
       borderRadius: BorderRadius.circular(15.0),
@@ -70,10 +80,16 @@ Widget tarjetas(String pURL, String pNombre, bool pEstado) {
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        ClipRRect(
-          child: Image.network(pURL),
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(15.0), topRight: Radius.circular(15.0)),
+        FittedBox(
+          fit: BoxFit.fill,
+          child: ClipRRect(
+            child: Image.network(
+              pURL,
+            ),
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(15.0),
+                topRight: Radius.circular(15.0)),
+          ),
         ),
         Padding(
           padding: EdgeInsets.all(10.0),
@@ -83,10 +99,7 @@ Widget tarjetas(String pURL, String pNombre, bool pEstado) {
             children: <Widget>[
               Text(
                 pNombre,
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                ),
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
               ),
               Text(
                 pEstado ? "Abieto" : "Cerrado",
